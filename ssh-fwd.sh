3ba9495690670c1eef7b902ea7d39a6036d09cfa
martin <fengmushu@gmail.com>
2024-03-26 08:24:30 +0000

    init

---
+++ ssh-fwd.sh
@@ -0,0 +1,13 @@
+
+
+ssh -o CheckHostIP=yes \
+	-o IdentityFile=/home/orangepi/.ssh/xh_id_rsa \
+	-o LogLevel=DEBUG \
+	-o ServerAliveCountMax=3 \
+	-o ServerAliveInterval=5 \
+	-o StrictHostKeyChecking=accept-new \
+	-o TCPKeepAlive=yes \
+	-o VerifyHostKeyDNS=yes \
+	-o ExitOnForwardFailure=yes \
+	-o BatchMode=yes \
+	-g -nN -R *:12522:localhost:22 -p 12580 ken@117.48.195.14
